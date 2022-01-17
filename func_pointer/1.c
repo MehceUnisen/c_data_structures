d5f0b23aca21416b8bfc8fb0611e19ff6b77c468
MehceUnisen <mehceu@gmail.com>
2022-01-17 15:38:47 +0300

    simple hashmap, looks pretty cool tho ngl

--- func_pointer/1.c
+++ func_pointer/1.c
@@ -5,13 +5,14 @@ void *helloFunc(){
 	return NULL;
 }
 
-void funcIterator(void (*param_func)(), int iter){
+void func_iterator(void (*param_func)(), int iter){
 	for(int i = 0; i < iter; i++){
 		param_func();
 	}
 }
 
 int main(){
-	funcIterator(helloFunc, 31);	
+    func_iterator(helloFunc, 31);	
 	return 0;
 }
+
